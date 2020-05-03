941844abc1822dd88888f1b1edfe765e101241b3
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2020-05-03 16:58:20 +0300

    Calculating winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,23 +2,51 @@ import React, {useState} from "react";
 import { Square } from "./Square";
 
 
+const calculateWinner = (squares) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+};
+
 export const Board = () => {
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [isXNext, setIsXNext] = useState(true);
+
     const handleSquareClick = (i) => {
         const squaresCopy = [...squares];
+        if (calculateWinner(squaresCopy) || squaresCopy[i]) {
+            return;
+        }
         squaresCopy[i] = isXNext ? 'X' : 'O';
         setIsXNext(!isXNext);
         setSquares(squaresCopy);
     };
     const renderSquare = (i) => {
-        return (
-            <Square
-                value={squares[i]}
-                onClick={() => handleSquareClick(i)}
-            />);
+        return <Square value={squares[i]} onClick={() => handleSquareClick(i)}/>
     };
-    const status = `Next player: ${(isXNext ? 'X' : 'O')}`;
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = `Winner is ${winner} !`;
+    } else {
+        status = `Next player: ${isXNext ? 'X': 'O'}`
+    }
+
     return (
         <div>
             <div className="status">{status}</div>
@@ -32,5 +60,5 @@ export const Board = () => {
                 {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
             </div>
         </div>
-    )
+    );
 };
