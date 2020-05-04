911a46a37b231d0aca625dcd026e39e714261695
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2020-05-04 10:19:17 +0300

    Storing history of the moves and lifting up state to the Game component
    Adding missing file for previous commit

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,55 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 import { Square } from "./Square";
 
 
-const calculateWinner = (squares) => {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-};
-
-export const Board = () => {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [isXNext, setIsXNext] = useState(true);
-
-    const handleSquareClick = (i) => {
-        const squaresCopy = [...squares];
-        if (calculateWinner(squaresCopy) || squaresCopy[i]) {
-            return;
-        }
-        squaresCopy[i] = isXNext ? 'X' : 'O';
-        setIsXNext(!isXNext);
-        setSquares(squaresCopy);
-    };
+export const Board = ({squares, onClick}) => {
     const renderSquare = (i) => {
-        return <Square value={squares[i]} onClick={() => handleSquareClick(i)}/>
+        return <Square value={squares[i]} onClick={() => onClick(i)}/>
     };
-
-    const winner = calculateWinner(squares);
-    let status;
-    if (winner) {
-        status = `Winner is ${winner} !`;
-    } else {
-        status = `Next player: ${isXNext ? 'X': 'O'}`
-    }
-
     return (
         <div>
-            <div className="status">{status}</div>
             <div className="board-row">
                 {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
             </div>
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,41 @@
-import React from "react";
-import {Board} from "./Board";
+import React, {useState} from "react";
+import { calculateWinner } from "../calculateWinner";
+import { Board } from "./Board";
 
 export const Game = () => {
+    const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+    const [isXNext, setIsXNext] = useState(true);
+
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    const handleSquareClick = (i) => {
+
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = isXNext ? 'X' : 'O';
+        setIsXNext(!isXNext);
+        setHistory(history.concat([{squares: squares}]));
+    };
+
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+        status = `Winner is ${winner} !`;
+    } else {
+        status = `Next player: ${isXNext ? 'X' : 'O'}`
+    }
+
     return (
         <div className="game">
             <div className="game-board">
-                <Board />
+                <Board
+                    squares={current.squares}
+                    onClick={(i) => handleSquareClick(i)}
+                />
             </div>
             <div className="game-info">
-                <div>{/* status */}</div>
+                <div>{status}</div>
                 <ol>{/* TODO */}</ol>
             </div>
         </div>
